9795b6ed2d7c7961a4519962b4f98d32469c13d5
daniellribeiro <daniellopes03@hotmail.com>
2022-06-27 20:53:49 -0300

    aula270622

--- TDD/src/CalculadoraTest.java
+++ TDD/src/CalculadoraTest.java
@@ -1,32 +1,30 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+    
 
+	Calculadora c = new Calculadora();
+	
 	@Test
 	public void somarDoisValoresPositivos() {
-		Calculadora c = new Calculadora();
 		
 		Assertions.assertEquals(7,c.somar(4,3));
 	}
 	
 	@Test
 	public void somarDoisValoresNegativos() {
-		Calculadora c = new Calculadora();
-		
 		Assertions.assertEquals(-7,c.somar(-4,-3));
 	}
 	
 	@Test
 	public void somarUmValorPositivoEOutroNegativo() {
-		Calculadora c = new Calculadora();
-		
 		Assertions.assertEquals(9,-3,12);
 	}
 	
 	@Test
 	public void somarDoisValoresZero() {
-		Calculadora c = new Calculadora();
 		
 		Assertions.assertEquals(0,c.somar(0,0));
 	}
