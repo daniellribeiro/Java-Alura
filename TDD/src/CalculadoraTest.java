17e5d6565e5a5fb1e05e4bf7d976fd61d92ca94e
daniellribeiro <daniellopes03@hotmail.com>
2022-06-28 22:05:39 -0300

    aula280622

--- TDD/src/CalculadoraTest.java
+++ TDD/src/CalculadoraTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -6,6 +9,26 @@ public class CalculadoraTest {
     
 
 	Calculadora c = new Calculadora();
+	
+	@BeforeAll
+	public static void inicializarTudo() {
+		System.out.println("inicializar tudo");
+	}
+	
+	@BeforeEach
+	public void inicializarMetodo() {
+		System.out.println("inicializar metodo");
+	}
+	
+	@AfterAll
+	public static void finalizarTudo() {
+		System.out.println("finalizar tudo");
+	}
+	
+	@AfterEach
+	public void finalizarMetodo() {
+		System.out.println("finalizar metodo");
+	}
 	
 	@Test
 	public void somarDoisValoresPositivos() {
